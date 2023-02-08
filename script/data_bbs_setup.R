e654b2100dd2b1d315512012a5737ca679bc7432
ae12345678 <adam.easey@uea.ac.uk>
2023-02-08 12:39:44 +0000

    adjusted bbs setup to save the stratify.

--- script/data_bbs_setup.R
+++ script/data_bbs_setup.R
@@ -2,6 +2,6 @@ library(bbsBayes)#produced using jags version 4.3.1
 fetch_bbs_data()#check https://rappdirs.r-lib.org/ for file location
 
 load_bbs_data()
-stratify(by="state")
+strat_bbs <- stratify(by="state")
 strat_state<-load_map(stratify_by = "state")
 plot(strat_state)
