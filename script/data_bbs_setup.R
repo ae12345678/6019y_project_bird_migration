52289c9f8f9b9788c0041fd8d2a5c9cfb07f3da1
ae12345678 <adam.easey@uea.ac.uk>
2023-02-11 18:04:49 +0000

    separated tables in stratified bbs data list to allow for use in ggplot etc.

--- script/data_bbs_setup.R
+++ script/data_bbs_setup.R
@@ -3,5 +3,9 @@ fetch_bbs_data()#check https://rappdirs.r-lib.org/ for file location
 
 load_bbs_data()
 strat_bbs <- stratify(by="state")
-strat_state<-load_map(stratify_by = "state")
+strat_state<-load_map(stratify_by = "state")#visual of stratification.
 plot(strat_state)
+bird_strat<-data.frame(strat_bbs[["bird_strat"]])#needed to use data in ggplot
+                                                 #turns list into dataframe.
+species_strat<-data.frame(strat_bbs[["species_strat"]])
+species_strat$aou <- as.numeric(as.character(species_strat$aou))#this must be run to convert a bad choice to a good choice.
