d2d190277713e0fa02a6f5ffc07fb840fab63d3c
ae12345678 <adam.easey@uea.ac.uk>
2023-02-12 12:45:25 +0000

    test for looping lm added.

--- script/data_bbs_extraction+analysis_test.R
+++ script/data_bbs_extraction+analysis_test.R
@@ -9,3 +9,9 @@ s<-bird_strat%>%
   filter (AOU == species_strat[86,2])%>%
   lm(formula=Year~SpeciesTotal)
 broom::tidy(s)
+#testing loop
+for (i in 1:(nrow(species_strat))){
+  try({s<-bird_strat%>%                   #try lets us skip the lots of 0 cases errors
+    filter (AOU == species_strat[i,2])%>%
+    lm(formula=Year~SpeciesTotal)})
+}
