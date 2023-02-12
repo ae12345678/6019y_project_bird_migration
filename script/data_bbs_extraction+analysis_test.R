50ab47f48246f8eca5af6ebad0e0372fee888681
ae12345678 <adam.easey@uea.ac.uk>
2023-02-12 12:34:17 +0000

    added test code for linear model

--- script/data_bbs_extraction+analysis_test.R
+++ script/data_bbs_extraction+analysis_test.R
@@ -4,3 +4,8 @@ bird_strat%>%
   filter (AOU == species_strat[86,2])%>%
   ggplot(aes(x=Year, y=SpeciesTotal))+
   geom_point()
+#testing linear models on bbs data.
+s<-bird_strat%>%
+  filter (AOU == species_strat[86,2])%>%
+  lm(formula=Year~SpeciesTotal)
+broom::tidy(s)
