e5f75c0baf658a7f19d15c905e6ce3f3df9481e2
ae12345678 <adam.easey@uea.ac.uk>
2023-02-16 18:34:09 +0000

    created the large scale automated sig. test.

--- script/data_bbs_extraction+analysis_test.R
+++ script/data_bbs_extraction+analysis_test.R
@@ -52,3 +52,21 @@ for (i in 1:10){
     }
   })    #NAs in col2 seem to mean not enough data in the model.
 }
+
+
+#large scale trialing of final automation (this crashed for me at 4GB ram.)
+sig_dir_bbs_test<-data.frame(matrix(ncol=2))
+for (i in 1:nrow(species_strat)){
+  try({s<-bird_strat%>%                   #try lets us skip the lots of 0 cases errors
+    filter (AOU == species_strat[i,2])%>%
+    lm(formula=Year~SpeciesTotal)#linear model
+  stidy <- broom::tidy(s)
+  sig_dir_bbs_test[nrow(sig_dir_bbs_test)+1,1]=species_strat[i,2]
+  if (stidy[2,5] <= 0.05& stidy[2,2]<0){        #if statements for simple presence and direction of change
+    sig_dir_bbs_test[nrow(sig_dir_bbs_test),2]="-"}else if(stidy[2,5] <= 0.05){
+      sig_dir_bbs_test[nrow(sig_dir_bbs_test),2]="+" 
+    }else{
+      sig_dir_bbs_test[nrow(sig_dir_bbs_test),2]="="  
+    }
+  })    #NAs in col2 seem to mean not enough data in the model.
+}
