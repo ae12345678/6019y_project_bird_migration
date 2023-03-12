e8b93e0ffbc3ff1544d88e71037b3e0c968a0df2
ae12345678 <adam.easey@uea.ac.uk>
2023-03-12 12:51:35 +0000

    new ebird setup

---
+++ script/data_ebird_us_setup.R
@@ -0,0 +1,15 @@
+library(auk)
+ebd <- auk_ebd("data/ebird/ebd_US_relJan-2023.txt", 
+               file_sampling = "data/ebird/ebd_sampling_relJan-2023.txt")
+ebird_filter <- ebd%>%
+  auk_date(date = c("2021-1-1","2022-1-1"))%>%
+  auk_protocol(protocol = c("Stationary", "Traveling")) %>% 
+  auk_country(country = c("US"))%>%#making sure only us data, especially in sampling
+  auk_complete()
+ebird_filter
+
+f_ebd <- file.path("data/ebird/ebd_us_filtered.txt")
+f_sampling <- file.path("data/ebird/sampling_us_filtered.txt")
+
+if (!file.exists(f_ebd)) {
+  auk_filter(ebird_filter, file = f_ebd, file_sampling = f_sampling)}
