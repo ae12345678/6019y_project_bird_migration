5809e2b5d45e382ef9e4d8f89e057aa07e89f00b
ae12345678 <adam.easey@uea.ac.uk>
2023-03-15 16:26:09 +0000

    created code to use state.abb based search in the ebird data.

--- script/data_ebird_us_analysis_test.R
+++ script/data_ebird_us_analysis_test.R
@@ -2,8 +2,13 @@ library(auk)
 library(tidyverse)
 f_select <- "data/ebird/ebd_smaller.txt"
 ebd_us<- read.table(file = f_select, sep="\t", header = TRUE)
+state_code <- state.abb
 genus_species[1,2]
+state_code[21]
 
 ebd_us%>%
   filter(SCIENTIFIC.NAME==genus_species[723,2])%>%
+  head()
+ebd_us%>%
+  filter(STATE.CODE==paste("US",state_code[21],sep="-"))%>%
   head()
