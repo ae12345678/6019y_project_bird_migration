5be6021fbcf1585f4df74134e1b8223f6ce18b65
ae12345678 <adam.easey@uea.ac.uk>
2023-03-25 16:15:44 +0000

    added code to include urban pop proportion 2010 into the state area analysis

--- script/data_state_analysis.R
+++ script/data_state_analysis.R
@@ -11,4 +11,13 @@ for (i in 1:50){
 
 state_area_analysis%>%
   ggplot(aes(x=X1,y=X2))+
-  geom_point()
+  geom_point(aes(color=X4))
+
+state.name[grep(as.character(state.abb[20]),state.abb)]
+for (i in 1:50){
+  state_area_analysis[i,1]=state_code[i]
+  state_area_analysis[i,2]=sum(as.numeric(bbs_ebird_data[,2])*as.numeric(bbs_ebird_data[,i+2]))/sum(bbs_ebird_data[i+2])
+  o<-filter(state_urban_pop,NA.==state.name[grep(as.character(state.abb[i]),state.abb)])
+  state_area_analysis[i,4]=sum(o$NA..2)
+}
+
