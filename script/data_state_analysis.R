22e3c3bf25f63ac44c59aaf75a3d0a3ec4666a8d
ae12345678 <adam.easey@uea.ac.uk>
2023-03-26 11:58:48 +0100

    added protected area cover

--- script/data_state_analysis.R
+++ script/data_state_analysis.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(xlsx)
 state_area_analysis<-data.frame(matrix(ncol=10))#creates dataframe for state analysis
 state_urban_pop <-read.xlsx(file.path("data/pop-urban-pct-historical.xls"),sheetName = "States",colIndex = c(2,15,16))
-
+state_GAP_protected <- read.csv(file.path("data/data_state_GAP_protected.csv"))
 for (i in 1:50){
   state_area_analysis[i,1]=state_code[i]
   state_area_analysis[i,2]=sum(as.numeric(bbs_ebird_data[,2])*as.numeric(bbs_ebird_data[,i+2]))/sum(bbs_ebird_data[i+2])
@@ -11,7 +11,7 @@ for (i in 1:50){
 
 state_area_analysis%>%
   ggplot(aes(x=X1,y=X2))+
-  geom_point(aes(color=X4))
+  geom_point(aes(color=X5))
 
 state.name[grep(as.character(state.abb[20]),state.abb)]
 for (i in 1:50){
@@ -19,5 +19,7 @@ for (i in 1:50){
   state_area_analysis[i,2]=sum(as.numeric(bbs_ebird_data[,2])*as.numeric(bbs_ebird_data[,i+2]))/sum(bbs_ebird_data[i+2])
   o<-filter(state_urban_pop,NA.==state.name[grep(as.character(state.abb[i]),state.abb)])
   state_area_analysis[i,4]=sum(o$NA..2)
+  p<-filter(state_GAP_protected,state==state.abb[i])
+  state_area_analysis[i,5]=sum(p$GAP_protected_percent)
 }
 
